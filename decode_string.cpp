4235eb334c3c55875adf16a525ce1d509c1a5bef
jarintasnim2n1 <163668265+jarintasnim2n1@users.noreply.github.com>
2024-04-23 22:32:41 +0600

    Add files via upload
    added week5 codes

---
+++ decode_string.cpp
@@ -0,0 +1,36 @@
+//                                 ॐ नमः शिवाय
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        string ans;
+        for(int i=0; i<n;)
+        {
+            if(s[i+2] == '0' && s[i+3] != '0')
+            {
+                ans += 96+stoi(s.substr(i,2));
+                i += 3;
+            }
+            else
+            {
+                ans += 96+(s[i]-'0');
+                i++;
+            }
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
