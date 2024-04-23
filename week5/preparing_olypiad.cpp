e1447a57e08af1ba2333a6834bdf18eede6452c4
jarintasnim2n1 <163668265+jarintasnim2n1@users.noreply.github.com>
2024-04-23 22:45:23 +0600

    Add files via upload
    added week5 codes

---
+++ week5/preparing_olypiad.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+  int n, l, r, x;
+
+  while (cin >> n >> l >> r >> x)
+  {
+    int arr[n];
+    for (int i = 0; i < n; i++)
+      cin >> arr[i];
+
+    int ans = 0;
+
+    for (int i = 0; i < (1 << n); i++)
+    {
+      int mx = INT_MIN, mn = INT_MAX;
+      int sum = 0;
+
+      for (int j = 0; j < n; j++)
+      {
+        if (i & (1 << j))
+        {
+          sum += arr[j];
+          mx = max(mx, arr[j]);
+          mn = min(mn, arr[j]);
+        }
+      }
+
+      if (l <= sum && sum <= r && (mx - mn) >= x)
+        ans++;
+    }
+
+    cout << ans << "\n";
+  }
+
+  return 0;
+}
+    					 		  		 	  	  		 			 	
