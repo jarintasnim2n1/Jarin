f96346b660e3bb8590deb86eab7bb0bbf3d8db4b
jarintasnim2n1 <163668265+jarintasnim2n1@users.noreply.github.com>
2024-05-08 23:58:03 +0600

    Add files via upload
    added week7 codes

---
+++ week7/divisible_array.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+	ll t;
+ cin>>t;
+	while(t--)
+	{
+   ll n;
+   cin>>n;
+    for(int i=1;i<=n;i++){
+  cout<<2*i<<" ";
+    }
+    cout<<'\n';
+    }
+    return 0;
+}
